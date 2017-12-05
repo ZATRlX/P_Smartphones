6d6dfd74a5a5afa5c576cb3dc7684e9066199d2c
Imacutekayx <imkayxy@gmail.com>
2017-12-05 13:53:13 +0100

    Écriture des commentaires
    Écriture des commentaires du programme (méthodes, classes & parties importantes)

--- P_GestProj2_Interface/Form1.cs
+++ P_GestProj2_Interface/Form1.cs
@@ -1,9 +1,8 @@
-﻿/*
-Auteur : Loïc Rosset
-Modifications par : Pierric Ripoll, Loïc Rosset, Ricardo Olivera, Grégory Briand
-Description :   Programme permettant de sélectionner des Smartphones dans une liste de Smartphones (géré en externe dans une base de données)
-                Dans le cadre du Projet GestProj2
-*/
+﻿// ETML
+// Auteur : Loïc Rosset
+// Modifications par : Pierric Ripoll, Loïc Rosset, Ricardo Olivera, Grégory Briand
+// Date : 05.12.2017
+// Description :   Programme permettant de sélectionner des Smartphones dans une liste de Smartphones (géré en externe dans une base de données). Dans le cadre du Projet GestProj2
 using MySql.Data.MySqlClient;
 using System;
 using System.Windows.Forms;
@@ -12,10 +11,11 @@ using System.Diagnostics;
 
 namespace P_GestProj2_Interface
 {
-    /// <summary>
-    /// Form principale avec le datagridview où tous les smartphones sont affichés,
-    /// et les contrôles de tri et de filtres
-    /// </summary>
+    // ETML
+    // Auteur : Loïc Rosset
+    // Modifications par : Pierric Ripoll, Loïc Rosset, Ricardo Olivera, Grégory Briand
+    // Date : 05.12.2017
+    // Description :   Form principale avec le datagridview où tous les smartphones sont affichés, et les contrôles de tri et de filtres
     public partial class Form1 : Form
     {
         //string servant de connection à la base de donnée. Utilisé dans l'objet MySqlConnection.
@@ -39,6 +39,9 @@ namespace P_GestProj2_Interface
         DataView dv = new DataView();
         Panel p = new Panel();
         
+        /// <summary>
+        /// Execution lors du lancement de la Form1
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +52,7 @@ namespace P_GestProj2_Interface
             MySqlDataReader rdr2 = ExecuteQuery(@"DROP DATABASE IF EXISTS db_Smartphones;
                                                 CREATE DATABASE db_Smartphones CHARACTER SET utf8 COLLATE utf8_general_ci;
                                                 USE db_Smartphones;
+                                                //TODO importer le fichier .sql dans la base de donnée
                                                 db_smartphones < CSV\CreateDatabase.sql");
 
             //rajoute les composants de la fonction Contact au panel de cette même fonction
@@ -103,6 +107,7 @@ namespace P_GestProj2_Interface
 
             connection.Close();
 
+            //Filtre des OS
             MySqlDataReader rdrOS = ExecuteQuery(@"SELECT DISTINCT smaOS FROM t_smartphone ORDER BY smaOS;");
 
             while (rdrOS.Read())
@@ -110,6 +115,7 @@ namespace P_GestProj2_Interface
                 cbOS.Items.Add(rdrOS.GetString("smaOS"));
             }
 
+            //Filtre des marques
             MySqlDataReader rdrMarque = ExecuteQuery(@"SELECT DISTINCT smaMarque FROM t_smartphone ORDER BY smaMarque;;");
 
             while (rdrMarque.Read())
@@ -117,6 +123,7 @@ namespace P_GestProj2_Interface
                 cbMarque.Items.Add(rdrMarque.GetString("smaMarque"));
             }
 
+            //Filtres de la taille de l'écran
             MySqlDataReader rdrTaille = ExecuteQuery(@"SELECT DISTINCT smaTailleEcran FROM t_smartphone ORDER BY smaTailleEcran;;");
 
             while (rdrTaille.Read())
@@ -124,6 +131,7 @@ namespace P_GestProj2_Interface
                 cbTailleEcran.Items.Add(rdrTaille.GetString("smaTailleEcran") + " pouces");
             }
 
+            //Filtre de la RAM
             MySqlDataReader rdrRAM = ExecuteQuery(@"SELECT DISTINCT smaRAM FROM t_smartphone ORDER BY smaRAM;");
 
             while (rdrRAM.Read())
@@ -131,6 +139,7 @@ namespace P_GestProj2_Interface
                 cbRAM.Items.Add(rdrRAM.GetString("smaRAM"));
             }
 
+            //Filtre du processeur
             MySqlDataReader rdrCPU = ExecuteQuery(@"SELECT DISTINCT proNom FROM t_processeur ORDER BY proPerformances;");
 
             while (rdrCPU.Read())
@@ -138,10 +147,12 @@ namespace P_GestProj2_Interface
                 cbCPU.Items.Add(rdrCPU.GetString("proNom"));
             }
 
+            //Ajout des dates manuellement
             cbDate.Items.Add("2015");
             cbDate.Items.Add("2016");
             cbDate.Items.Add("2017");
             
+            //Ajout des comboboxes dans la panel et modification de celui-ci
             p.Controls.Add(cbCPU);
             p.Controls.Add(cbTailleEcran);
             p.Controls.Add(cbRAM);
@@ -157,10 +168,16 @@ namespace P_GestProj2_Interface
             p.Location = new System.Drawing.Point(10, 14);
         }
 
+        /// <summary>
+        /// Execute les commandes SQL dans la base de donnée
+        /// </summary>
+        /// <param name="query">commande à executer</param>
+        /// <returns>MySqlDataReader rdr</returns>
         private MySqlDataReader ExecuteQuery(string query)
         {
             MySqlCommand acmd = new MySqlCommand(query, connection);
 
+            //Se connecte à la base de donnée
             if (connection != null && connection.State == ConnectionState.Closed)
             {
                 connection.Open();
@@ -171,11 +188,17 @@ namespace P_GestProj2_Interface
                 rdr.Close();
             }
 
+            //Execute la commande
             rdr = acmd.ExecuteReader();
 
             return rdr;
         }
 
+        /// <summary>
+        /// Change les filtres
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         public void ChangeFilters(object sender, EventArgs e)
         {
             if (canChange)
@@ -196,6 +219,7 @@ namespace P_GestProj2_Interface
                 }
                 
                 DataSet ds = new DataSet();
+                //Réécrit le Datagridview avec les informations filtrées
                 MySqlDataAdapter daa = new MySqlDataAdapter(@"SELECT smaMarque AS Marque, smaModele AS Modele, smaDate AS DateSortie, smaOS AS OS, smaConstructeurs AS Constructeur, smaTailleEcran AS TailleEcran, smaAutonomie AS Autonomie, smaRAM AS RAM, proNom AS Processeur FROM t_smartphone INNER JOIN t_processeur ON t_smartphone.Idproc = t_processeur.idProcesseur;", connection);
                 connection.Close();
                 connection.Open();
@@ -203,6 +227,7 @@ namespace P_GestProj2_Interface
 
                 DataView dv;
                 
+                //remodèle certaines informations
                 if (cb.Name == "cbCPU")
                 {
                     dv = new DataView(ds.Tables[0], "Processeur  = '" + cb.Text + "'", "Processeur Desc", DataViewRowState.CurrentRows);
@@ -217,7 +242,6 @@ namespace P_GestProj2_Interface
                 }
                 else
                 {
-                    //dv = new DataView(ds.Tables[0], "sma" + cb.Name.Substring(2) + " = '" + cb.Text + "'", "sma" + cb.Name.Substring(2) + " Desc", DataViewRowState.CurrentRows);
                     dv = new DataView(ds.Tables[0], cb.Name.Substring(2) + " = '" + cb.Text + "'", cb.Name.Substring(2) + " Desc", DataViewRowState.CurrentRows);
                 }
                 
@@ -232,6 +256,11 @@ namespace P_GestProj2_Interface
             canChange = true;
         }
 
+        /// <summary>
+        /// Lorsque le Form1 charge, ajoute les informations des colonnes
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void Form1_Load(object sender, EventArgs e)
         {
             MySqlDataReader rdr2 = ExecuteQuery(@"SELECT smaDate, smaMarque, smaModele, smaOS, smaConstructeurs, smaTailleEcran, smaAutonomie, smaRAM FROM t_smartphone;");
@@ -251,6 +280,11 @@ namespace P_GestProj2_Interface
             connection.Close();
         }
 
+        /// <summary>
+        /// Affiche les informations des téléphones lotsque l'utilisateur clique sur une cellule
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void DgvResultatSmartphones_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             MySqlDataReader rdr4 = ExecuteQuery(@"SELECT smaMarque, smaModele, smaDate, smaOS, smaConstructeurs, smaTailleEcran, smaAutonomie, smaRAM, proNom FROM t_smartphone INNER JOIN t_processeur ON t_smartphone.Idproc = t_processeur.idProcesseur;");
@@ -267,37 +301,71 @@ namespace P_GestProj2_Interface
             connection.Close();
         }
 
+        /// <summary>
+        /// Affiche un pop-up avec le texte voulu
+        /// </summary>
+        /// <param name="show">Texte à afficher</param>
         public void PopUp(string show)
         {
             MessageBox.Show(show, "", MessageBoxButtons.OK);
         }
 
+        /// <summary>
+        /// Affiche le panel de contact lorsque le bouton contact est cliqué
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void BtnContact_Click(object sender, EventArgs e)
         {
             pnlContact.Enabled = true;
             pnlContact.Visible = true;
         }
 
+        /// <summary>
+        /// Ouvre une page internet vers le compte GitHub de Pierric Ripoll lorsque que l'utilisateur clic sur le lien
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void Pierric_Click(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(PIERRIC);
         }
 
+        /// <summary>
+        /// Ouvre une page internet vers le compte GitHub de Ricardo Cardoso Oliveira lorsque que l'utilisateur clic sur le lien
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void Ricardo_Click(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(RICARDO);
         }
 
+        /// <summary>
+        /// Ouvre une page internet vers le compte GitHub de Loïc Rosset lorsque que l'utilisateur clic sur le lien
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void Loic_Click(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(LOIC);
         }
 
+        /// <summary>
+        /// Ouvre une page internet vers le compte GitHub de Grégory Briand lorsque que l'utilisateur clic sur le lien
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void Gregory_Click(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(GREGORY);
         }
 
+        /// <summary>
+        /// Ferme le panel de contact lorsque l'utilisateur clique sur le bouton de fermeture du panel
+        /// </summary>
+        /// <param name="sender">Objet envoyant la requête</param>
+        /// <param name="e">Evenement déclanchant cette méthode</param>
         private void BtnStopContact_Click(object sender, EventArgs e)
         {
             pnlContact.Enabled = false;
